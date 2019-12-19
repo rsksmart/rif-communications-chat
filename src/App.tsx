890e74c86ad94d6bb9ec002e44a82642bb66d83d
Juraj Piar <juraj.piar@hotmail.com>
2019-12-19 15:57:13 +0000

    feat!: removes old structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import React from "react";
-import { Router } from "react-router-dom";
+import React from 'react';
+import { Router } from 'react-router-dom';
 
-import UserProvider from "providers/UserProvider";
-import Routes from "views/Routes";
-import Header from "components/Header";
+import UserProvider from 'providers/UserProvider';
+import Routes from 'views/Routes';
+import Header from 'components/Header';
 // import Footer from "components/Footer";
-import { history } from "routes";
+import { history } from 'routes';
 
 console.log(process.env.REACT_APP_RNS_SERVER);
 
 const App: React.FC = () => {
   return (
-    <UserProvider.Provider>
-      <Router history={history}>
-        <div
-          style={{
-            minHeight: "100vh",
-            display: "flex",
-            flexDirection: "column"
-          }}
-        >
-          <Header />
+    // <UserProvider.Provider>
+    <Router history={history}>
+      <div
+        style={{
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Header />
 
-          {/* Content of the dapp*/}
-          <div style={{ paddingTop: "1em" }}>
-            <Routes />
-          </div>
-          <div style={{ flexGrow: 1 }} />
-
-          {/*<Footer />*/}
+        {/* Content of the dapp*/}
+        <div style={{ paddingTop: '1em' }}>
+          <Routes />
         </div>
-      </Router>
-    </UserProvider.Provider>
+        <div style={{ flexGrow: 1 }} />
+
+        {/*<Footer />*/}
+      </div>
+    </Router>
+    // </UserProvider.Provider>
   );
 };
 
--- src/views/NotFound.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export default () => <p>The page you requested could not be found</p>;
