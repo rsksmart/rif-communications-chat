affc0a287e463ea7eaa5963d195ab0335ba39ea7
Juraj Piar <juraj.piar@hotmail.com>
2020-01-08 18:42:35 +0000

    refactor: renames js indexing files to ts

--- src/models/index.js
+++
@@ -1,4 +0,0 @@
-import User from './User';
-import Contact from './Contact';
-
-export { User, Contact };
---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+import User from './User';
+import Contact from './Contact';
+import Message from './Message';
+
+export { User, Contact, Message };
