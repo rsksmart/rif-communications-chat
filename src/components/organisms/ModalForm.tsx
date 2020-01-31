d0867815b8c82691c0d1779787583e5881a17e8e
Juraj Piar <juraj.piar@hotmail.com>
2020-01-31 14:26:52 +0000

    refactor: removes unneeded fragment

--- src/components/organisms/ModalForm.tsx
+++ src/components/organisms/ModalForm.tsx
@@ -1,16 +1,16 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext } from 'react'
 
-import Button from 'components/atoms/buttons/Button';
-import { Form } from 'components/atoms/forms';
-import { ModalProps } from 'components/atoms/modal/Modal';
-import ModalDialogue from 'components/molecules/ModalDialogue';
-import UserStore from 'store/User/UserStore';
+import Button from 'components/atoms/buttons/Button'
+import { Form } from 'components/atoms/forms'
+import { ModalProps } from 'components/atoms/modal/Modal'
+import ModalDialogue from 'components/molecules/ModalDialogue'
+import UserStore from 'store/User/UserStore'
 
 export interface ModalFormProps {
-  formik: any;
-  modalProps: ModalProps;
-  submitBtnLabel: string;
-  title: string;
+  formik: any
+  modalProps: ModalProps
+  submitBtnLabel: string
+  title: string
 }
 
 const ModalForm: FC<ModalFormProps> = ({
@@ -26,31 +26,29 @@ const ModalForm: FC<ModalFormProps> = ({
         message: { isLoading },
       },
     },
-  } = useContext(UserStore);
+  } = useContext(UserStore)
 
   return (
     <Form>
       <ModalDialogue
         title={title}
         footer={
-          <>
-            <Button
-              variant="primary"
-              type="button"
-              className="ml-auto justify-content-end"
-              disabled={isLoading || !formik.isValid}
-              onClick={formik.submitForm}
-            >
-              {submitBtnLabel}
-            </Button>
-          </>
+          <Button
+            variant="primary"
+            type="button"
+            className="ml-auto justify-content-end"
+            disabled={isLoading || !formik.isValid}
+            onClick={formik.submitForm}
+          >
+            {submitBtnLabel}
+          </Button>
         }
         {...modalProps}
       >
         {children}
       </ModalDialogue>
     </Form>
-  );
-};
+  )
+}
 
-export default ModalForm;
+export default ModalForm
